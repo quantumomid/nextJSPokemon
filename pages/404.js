20ce1d7b8fb15cca4b075105cb24b1fb9d51ed89
quantumomid <omid.wakili@outlook.com>
2021-12-10 20:27:27 +0000

    redirect user to home page within 3 seconds of 404 component mounting

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 
 const NotFound = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
+
     return (
         <article className="not-found">
             <h1>Oh no.......</h1>
             <p>The page you are looking for does not exist.</p>
             <p>Go back to the <Link href="/"><a>Homepage</a></Link> and try again!</p>
         </article>
-    )
+    );
 }
 
 export default NotFound;
