11bdc69a2fe84da519a0d9828e0364454744a05a
quantumomid <omid.wakili@outlook.com>
2021-12-10 20:52:21 +0000

    add metadata to the About and Pokemons pages using Next.js' Head component

--- pages/pokemons/index.js
+++ pages/pokemons/index.js
@@ -1,7 +1,12 @@
+import Head from "next/head";
 
 const Pokemons = () => {
     return (
         <div>
+            <Head>
+                <title>Pokemon's Den | Pokemons</title>
+                <meta name="keywords" content="pokemons" />
+            </Head>
             <h1>All Pokemons</h1>
         </div>
     )
