879d87f472b6235fd5ffe218be6a9be2e7792b1f
quantumomid <omid.wakili@outlook.com>
2021-12-10 22:45:41 +0000

    use pokemon api to fetch 100 pokemon names and render into article components with some simple styling

--- pages/pokemons/index.js
+++ pages/pokemons/index.js
@@ -1,6 +1,21 @@
 import Head from "next/head";
+import styles from "../../styles/Pokemons.module.css";
 
-const Pokemons = () => {
+export const getStaticProps = async() => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+    const data = await response.json();
+    const hundredPokemons = data.results;
+
+    return {
+        props: {
+            pokemons: hundredPokemons
+        }
+    }
+}
+
+const Pokemons = ({ pokemons }) => {
+    console.log(pokemons);
+
     return (
         <div>
             <Head>
@@ -8,6 +23,15 @@ const Pokemons = () => {
                 <meta name="keywords" content="pokemons" />
             </Head>
             <h1>All Pokemons</h1>
+            { pokemons.map(pokemon => (
+                <article key={pokemon.name} className={styles.single}>
+                    <a>
+                        <h3>{ pokemon.name }</h3>
+                    </a>
+                </article>
+                )
+                ) 
+            }
         </div>
     )
 }
