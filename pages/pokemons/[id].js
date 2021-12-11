fccfd160b73f7ef0d879424da7160283a75f7e64
quantumomid <omid.wakili@outlook.com>
2021-12-11 09:27:46 +0000

    use individual pokemon data to render some info on each route

--- pages/pokemons/[id].js
+++ pages/pokemons/[id].js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { getHundredPokemonsWithIds, getSinglePokemonDetails } from "../../utils/pokemonUtils";
+import Image from "next/image";
 
 export const getStaticPaths = async() => {
     const hundredPokemonsWithIds = await getHundredPokemonsWithIds();
@@ -29,6 +30,9 @@ export const getStaticProps = async (context) => {
 }
 
 const PokemonDetails = ({ pokemon }) => {
+    console.log(pokemon);
+    const types = pokemon.types.map(type => <li key={type.slot}>{ type.type.name }</li>)
+    const abilities = pokemon.abilities.map(ability => <li key={ability.slot}>{ability.ability.name}</li>)
     return (
         <React.Fragment>
             <Head>
@@ -37,6 +41,17 @@ const PokemonDetails = ({ pokemon }) => {
             </Head>
             <article>
                 <h1>{ pokemon.name }</h1>
+                {/* <Image src={pokemon.sprites.front_default} alt={`Font view of the pokemon ${pokemon.name}`} width={100} height={100} layout="responsive"/> */}
+                <img src={pokemon.sprites.front_default} alt={`Font view of the pokemon ${pokemon.name}`} width={100} height={100}/>
+                <p>Weight: { pokemon.weight }</p>
+                <h2>Types:</h2>
+                <ul>
+                    { types }
+                </ul>
+                <h2>Abilities: </h2>
+                <ul>
+                    { abilities }
+                </ul>
             </article>
         </React.Fragment>
     )
